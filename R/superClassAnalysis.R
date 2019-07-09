48938da4ca74a523a7c08a5ee719b49c6bcd9164
MBalthasar <maphi@maxi-dsl.de>
2019-07-09 18:03:34 +0200

    Adjusted description.

--- R/superClassAnalysis.R
+++ R/superClassAnalysis.R
@@ -2,11 +2,12 @@
 #'
 #' This R package contains several functions which are designed to increase the accurcacy
 #' of a supervised classification. The functions are based on the \code{\link[RStoolbox]{superClass}}
-#' function from the \code{\link[RStoolbox]} package by Benjamin Leutner
+#' function from the \href{https://bleutner.github.io/RStoolbox/}{RStoolbox} package by Benjamin Leutner
 #' \url{benjamin.leutner@uni-wuerzburg.de}.
 #'
 #' @details Functions included in this package are:
 #' \itemize{
+#'   \item \code{\link{LoadPackages}}
 #'   \item \code{\link{SampleSaturation}}
 #'   \item \code{\link{ResolutionSaturation}}
 #'   \item \code{\link{PolygonSaturation}}
